06ae3a13edd21a3861b0eb4b59302b5207b35b0f
Micaela Pinheiro <micaela.mews@gmail.com>
2022-06-24 11:51:31 -0300

    implement stop animation at the end of the game

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector(".super-mario");
 const pipe = document.querySelector(".pipe-game");
+const stopAnimation =  document.querySelectorAll(".stop-animation")
 
 const jump = () => {
   mario.classList.add("jump-mario");
@@ -16,13 +17,14 @@ const loopGame = setInterval(() => {
     .bottom.replace("px", "");
 
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
     mario.style.bottom = `${marioPosition}px`;
-
     mario.src = "./Images/mario-game-over.png";
     mario.classList.add('game-over')
-    mario.style.marginLeft = "80px";
+    mario.style.marginLeft = "80px";  
+    for(let i in stopAnimation){
+      stopAnimation[i].style.left = `${stopAnimation[i].offsetLeft}px`
+      stopAnimation[i].style.animation =  "none" 
+    }
 
     clearInterval(loopGame);
   }
