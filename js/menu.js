332aa5bfb69c5a9f3dcce83b548a06ea764ebbce
Micaela Pinheiro <micaela.mews@gmail.com>
2022-07-01 19:03:48 -0300

    error handling when getting HTML element and scope error

--- js/menu.js
+++ js/menu.js
@@ -18,24 +18,25 @@ class createElement {
         return document.body.insertBefore(this.elementName, null);
     }
 }
-export function createMenu (){
-    let game = document.querySelector('.game')
-    let container = game.appendChild(new createElement('container', 'div'))
-    let templateMenu  = container.appendChild(new createElement("templateMenu", "div"))
-    templateMenu.innerHTML = `<h1><span>Super</span> Mario StartSe.</h1>`
-    let menuButton = container.appendChild(new createElement("menuButton", "nav"))
-    menuButton.innerHTML = `<ul>
+function createMenu(){
+        var game = document.querySelector('.game')
+        var container = game.appendChild(new createElement('container', 'div'))
+        var templateMenu  = container.appendChild(new createElement("templateMenu", "div"))
+        templateMenu.innerHTML = `<h1><span>Super</span> Mario StartSe.</h1>`
+        var menuButton = container.appendChild(new createElement("menuButton", "nav"))
+        menuButton.innerHTML = `<ul>
                             <li id="starGame">Start</li>
                         </ul>`
-
 }
 createMenu()
 starGame.addEventListener('click', ()=>{
-    container.style.display = 'none'
+    document.querySelector('.container').style.display = 'none'
     mario.src = '../Images/super-mario.gif'
     pipe.style.display = ""
     for(let i in stopAnimation){
-        stopAnimation[i].style.animation =  "" 
+        if(typeof(stopAnimation[i]) == 'object'){
+            stopAnimation[i].style.animation =  "" 
+        }
       }
     
 })
