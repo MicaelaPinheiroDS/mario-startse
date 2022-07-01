59c7778293445826a33bfdde6bd032d11e82063d
Micaela Pinheiro <micaela.mews@gmail.com>
2022-07-01 18:42:59 -0300

    export var and music implementation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const mario = document.querySelector(".super-mario");
-const pipe = document.querySelector(".pipe-game");
+export const mario = document.querySelector(".super-mario");
+export const pipe = document.querySelector(".pipe-game");
 const goomba = document.querySelector(".goomba")
-const stopAnimation =  document.querySelectorAll(".stop-animation")
-
+export const stopAnimation =  document.querySelectorAll(".stop-animation")
+const backgroundSongs = document.querySelector(".background-songs")
 const jump = () => {
   mario.classList.add("jump-mario");
 
@@ -21,14 +21,15 @@ const loopGame = setInterval(() => {
   if ((pipePosition <= 120 && pipePosition > 0 && marioPosition < 117.5) || (goombaPosition <= 120 && goombaPosition > 0 && marioPosition < 117.5)) {
     mario.style.bottom = `${marioPosition}px`;
     mario.src = "./Images/mario-game-over.png";
+    backgroundSongs.src = "./songs/game-over.mp3"
+    backgroundSongs.loop = false
     mario.classList.add('game-over')
     mario.style.marginLeft = "80px";  
+    clearInterval(loopGame);
     for(let i in stopAnimation){
       stopAnimation[i].style.left = `${stopAnimation[i].offsetLeft}px`
       stopAnimation[i].style.animation =  "none" 
     }
-
-    clearInterval(loopGame);
   }
 }, 10);
 
